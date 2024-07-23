7d39bf34626224dd13b213bea90da2731e7d2be3
Jessica Sturgell <sturgell01@gmail.com>
2024-07-23 13:23:38 -0400

    added fizzbuzz

---
+++ csharp/FizzBuzz/Program.cs
@@ -0,0 +1,27 @@
+﻿void Main()
+{
+    int n = 0;
+    do
+    {
+        n++;
+        if (n % 15 == 0)
+        {
+            Console.WriteLine("fizzbuzz");
+        }
+        else if (n % 3 == 0)
+        {
+            Console.WriteLine("fizz");
+        }
+        else if (n % 5 == 0)
+        {
+            Console.WriteLine("buzz");
+        }
+        else
+        {
+            Console.WriteLine(n);
+        }
+        ;
+    } while (n < 100);
+}
+
+Main();
